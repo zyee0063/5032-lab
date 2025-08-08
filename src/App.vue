f312575a24b451bc49ff8a3110ecb9e3e0eec78d
zyee0063 <zyee0063@student.monash.edu>
2025-08-08 11:02:22 +0800

    Trigger deployment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import JSONLab from './components/JSONLab.vue'
 import BHeader from './components/BHeader.vue'
-import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'// import LibraryDashboard from './components/LibraryDashboard.vue'
 </script>
 
 <template>
