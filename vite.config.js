9118befb44a3ba3151837a440551c1238739f84d
zyee0063 <zyee0063@student.monash.edu>
2025-08-08 10:40:03 +0800

    chore(ci): add gh-pages deploy workflow for master branch

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/5032-lab/',       // ✅ GitHub Pages 项目名
+  plugins: [vue(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
   }
 })
+
